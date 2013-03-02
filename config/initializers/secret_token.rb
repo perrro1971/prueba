ad486ec2e60f5aff6d545b200760b9ac2de4b01a
Walter Semelshtein <walters@minerva-group.com>
2013-03-02 16:14:26 -0300

    base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prueba::Application.config.secret_token = '51d223ddc06c78de84e60f3d71baeede8d7622489290a0b861dc990f421c8b0f6f24a89726255543eb8411ffdb9859698575c7a3ff3e78e6fe270d5fdbd316d4'
